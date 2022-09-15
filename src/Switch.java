7777fba22def82843e68012f6ba8ecb3a6a02414
Yuri-Jung <dbfl14433@gmail.com>
2022-09-15 16:28:34 +0900

    swith 수정

--- src/Switch.java
+++ src/Switch.java
@@ -19,49 +19,58 @@ public class Switch {
                 System.out.println("노력하세요");
                 break;
 
-            case 'F':
+//            case 'F':
+////                System.out.println("탈락입니다.");
+//                break;
+
+            default:
                 System.out.println("탈락입니다.");
                 break;
+
         }
         System.out.println("\n\n");
 //       자바의 날짜 관련 클래스
-        Calendar cal=Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();
         //현재 시간을 날짜 클래스에 적용
         cal.setTime(new Date());
 //        현재 날짜를 기준으로 일주일 중 오늘 요일을 가져온다.(숫자로)
 //        명령어다. 달력 클래스 new Date
-        int currDay=cal.get(Calendar.DAY_OF_WEEK);
+        int currDay = cal.get(Calendar.DAY_OF_WEEK);
 //         주중의 무슨 요일인지 숫자로 가져온다.
 
         System.out.println("변수 currDay의 값: " + currDay);
-        switch (currDay){
-            case 1:
-                System.out.println("오늘은 일요일입니다");
-                break;
+        switch (currDay) {
+//            case 1:
+//                System.out.println("오늘은 일요일입니다");
+//                break;
 
-            case 2 :
+            case 2:
                 System.out.println("오늘은 월요일입니다");
                 break;
 
-            case 3 :
+            case 3:
                 System.out.println("오늘은 화요일입니다");
                 break;
 
-            case 4 :
+            case 4:
                 System.out.println("오늘은 수요일입니다");
                 break;
 
-            case 5 :
+            case 5:
                 System.out.println("오늘은 목요일입니다");
                 break;
 
-            case 6 :
+            case 6:
                 System.out.println("오늘은 금요일입니다");
                 break;
 
-            case 7 :
-                System.out.println("오늘은 토요일입니다");
-                break;
+//            case 7 :
+//                System.out.println("오늘은 토요일입니다");
+//                break;
+
+            default:
+                System.out.println("오늘은 주말입니다");
+//                값이 없는 경우 디폴트값이 출력된다.        }
         }
     }
 }
